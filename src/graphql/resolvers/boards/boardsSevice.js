fe715bcdccd1923caa32d4b02bc79f3c2285d089
Vadym Kharyton <v-vkharyton@expediagroup.com>
2021-07-01 20:46:52 +0300

    review comments

--- src/graphql/resolvers/boards/boardsSevice.js
+++ src/graphql/resolvers/boards/boardsSevice.js
@@ -1,10 +1,12 @@
 const db = require('./boardsDAL')
 
+// these methods should be moved into index.js files -> methods from index.js should call methods from service file -> service file methods should cal DAL methods
 const getBoards = async (parent, args, context, info) => {
   const userBoards = await db.getBoardsByUserId(args.input)
   return userBoards
 }
 
+// replace unused parameters with underscores. Do it in all files
 const createBoard = async (parent, args, context, info) => {
   const newBoard = await db.createBoard(args.input)
   return newBoard
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const { ApolloServer } = require('apollo-server-hapi')
 const Hapi = require('@hapi/hapi')
+// remove unused variables
 const Boom = require('@hapi/boom')
 const { sequelize } = require('./infrastructure/database')
 
@@ -14,23 +15,27 @@ console.log(process.argv)
 console.log(`process.cwd`, process.cwd())
 
 const init = async () => {
+  // remove apolloserver, register graphql as here: https://www.apollographql.com/docs/apollo-server/v1/servers/hapi/ 
   const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
   })
   await server.start()
 
+  // remove 
   const app = Hapi.Server({
     host: HOST || 'localhost',
     port: PORT || 1234,
   })
 
+  // remove this
   await server.applyMiddleware({
     app,
   })
 
   await server.installSubscriptionHandlers(app.listener)
 
+  // create src/routes folder, and move all route register into separate files in this folder
   app.route([
     {
       method: 'GET',
